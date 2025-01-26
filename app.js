3814725774763ab4cdc97723fec2579ceaccad16
Gabriel <gabo18gc96@gmail.com>
2025-01-26 10:42:00 -0600

    Updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaAmigos = [];
+let numerosGenerados = [];
 const listaDeAmigos = document.querySelector("#listaAmigos");
 listaDeAmigos.innerHTML = "";
 
@@ -15,14 +16,39 @@ function agregarAmigo(){
         let nuevoElemento = document.createElement("li");
         nuevoElemento.textContent = document.querySelector("#amigo").value;
         listaDeAmigos.appendChild(nuevoElemento);
+        console.log(listaAmigos);
 
         document.querySelector("#amigo").value = "";
     }
 }
 
 function sortearAmigo(){
+    let numeroAleatorio = generarNumeroSecreto();
+    if(numeroAleatorio != -1){
+        let amigoSorteado = listaAmigos[numeroAleatorio];
+        listaDeAmigos.innerHTML = "";
+        document.querySelector("#resultado").innerHTML = `El amigo sorteado es ${amigoSorteado}`;
+    }
+}
+
+function generarNumeroSecreto(){
     let numeroAleatorio = Math.floor(Math.random() * listaAmigos.length);
-    let amigoSorteado = listaAmigos[numeroAleatorio];
-    listaDeAmigos.innerHTML = "";
-    document.querySelector("#resultado").innerHTML = `El amigo sorteado es ${amigoSorteado}`;
+
+    console.log(numeroAleatorio);
+
+    if(numerosGenerados.length == listaAmigos.length){
+        document.querySelector("#resultado").innerHTML = "No hay más amigos que sortear!";
+        alert("Ya se han sorteado todos los nombres existentes!");
+        return -1;
+    }
+    else{
+        if(numerosGenerados.includes(numeroAleatorio)){
+            return generarNumeroSecreto();
+        }
+        else{
+            numerosGenerados.push(numeroAleatorio);
+            console.log(numerosGenerados);
+            return numeroAleatorio;
+        }
+    }
 }
